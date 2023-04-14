e1008dd08a61b68faf3a23efa0619b399d6ca6d8
404pandas <113853550+404pandas@users.noreply.github.com>
2023-04-14 16:08:35 -0400

    Edited controllers, more character data formatting, Mongo partially linked. Switching gears to config

--- server/controllers/characterController.js
+++ server/controllers/characterController.js
@@ -1,46 +1,49 @@
+// import = asyncHandler(asyncandler
+const asyncHandler = require("express-async-handler");
+
 // Get all characters
 // GET /api/characters
 // Private
-const getCharacters = (req, res) => {
+const getCharacters = asyncHandler(async (req, res) => {
   if (!req.body.text) {
     res.status(400);
     throw new Error("please add a text field");
   }
   res.status(200).json({ message: "get characters" });
-};
+});
 
 // Create character
 // POST /api/characters
 // Private
-const createCharacter = (req, res) => {
+const createCharacter = asyncHandler(async (req, res) => {
   if (!req.body.text) {
     res.status(400);
     throw new Error("please add a text field");
   }
   res.status(200).json({ message: "Create character" });
-};
+});
 
 // Update Character
 // PUT /api/character/:id
 // Private
-const updateCharacter = (req, res) => {
+const updateCharacter = asyncHandler(async (req, res) => {
   if (!req.body.text) {
     res.status(400);
     throw new Error("please add a text field");
   }
   res.status(200).json({ message: `update character ${req.params.id}` });
-};
+});
 
 // Delete character
 // DELETE /api/character/:id
 // Private
-const deleteCharacter = (req, res) => {
+const deleteCharacter = asyncHandler(async (req, res) => {
   if (!req.body.text) {
     res.status(400);
     throw new Error("please add a text field");
   }
   res.status(200).json({ message: `delete character ${req.params.id}` });
-};
+});
 
 module.exports = {
   updateCharacter,
--- server/controllers/greetController.js
+++ server/controllers/greetController.js
@@ -1,46 +1,36 @@
+const asyncHandler = require("express-async-handler");
+
 // Get all greeting
 // GET /api/greeting
 // Private
-const getGreetings = (req, res) => {
-  if (!req.body.text) {
-    res.status(400);
-    throw new Error("please add a text field");
-  }
+const getGreetings = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "get greetings" });
-};
+});
 
 // Create greeting
 // POST /api/greeting
 // Private
-const createGreeting = (req, res) => {
-  if (!req.body.text) {
-    res.status(400);
-    throw new Error("please add a text field");
-  }
+const createGreeting = asyncHandler(async (req, res) => {
   res.status(200).json({ message: "Create greeting" });
-};
+});
 
 // Update greeting
 // PUT /api/greeting/:id
 // Private
-const updateGreeting = (req, res) => {
-  if (!req.body.text) {
-    res.status(400);
-    throw new Error("please add a text field");
-  }
-  res.status(200).json({ message: `update greeting ${req.params.id}` });
-};
+const updateGreeting = asyncHandler(
+  (async = (req, res) => {
+    res.status(200).json({ message: `update greeting ${req.params.id}` });
+  })
+);
 
 // Delete greeting
 // DELETE /api/greeting/:id
 // Private
-const deleteGreeting = (req, res) => {
-  if (!req.body.text) {
-    res.status(400);
-    throw new Error("please add a text field");
-  }
-  res.status(200).json({ message: `delete greeting ${req.params.id}` });
-};
+const deleteGreeting = asyncHandler(
+  (async = (req, res) => {
+    res.status(200).json({ message: `delete greeting ${req.params.id}` });
+  })
+);
 
 module.exports = {
   getGreetings,
