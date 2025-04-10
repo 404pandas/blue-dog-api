f27b6e3a0c830741b276268500d713de8cb0f082
404pandas <mary.panda.jackson@gmail.com>
2025-04-10 08:55:48 -0400

    All restful routes working for public facing api

--- server/src/routes/api/index.ts
+++ server/src/routes/api/index.ts
@@ -8,11 +8,17 @@ import { locationRouter } from "./location-routes.js";
 
 const router = Router();
 
+// Tested and working 8:54AM 4/10
 router.use("/characters", characterRouter);
+// Tested and Working 8:55AM 4/10
 router.use("/shorts", shortRouter);
+// Tested and working 8:56AM 4/10
 router.use("/books", bookRouter);
+// Tested and working 8:53AM 4/10
 router.use("/episodes", episodeRouter);
+// Tested and working 8:52AM 4/10
 router.use("/items", itemRouter);
+// Tested and working 8:55AM 4/10
 router.use("/locations", locationRouter);
 
 export default router;
