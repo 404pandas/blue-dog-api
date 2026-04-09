b59625a52897027a187710d84aacab870d2f13be
404pandas <mary.panda.jackson@gmail.com>
2026-04-09 09:08:54 -0500

    adding logs

--- server/src/config/db.ts
+++ server/src/config/db.ts
@@ -3,9 +3,11 @@ dotenv.config();
 
 import mongoose from "mongoose";
 
-mongoose.connect(
-  process.env.MONGODB_URL || "mongodb://127.0.0.1:27017/blue-dog-api"
-);
+const uri = process.env.MONGODB_URL || "mongodb://127.0.0.1:27017/blue-dog-api";
+console.log("[db] MONGODB_URL set:", !!process.env.MONGODB_URL);
+console.log("[db] Connecting to:", uri.replace(/:\/\/[^@]+@/, "://<credentials>@"));
+
+mongoose.connect(uri);
 
 const db = mongoose.connection;
 
